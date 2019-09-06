e3ebb99e57c4151fbc42fe86be00cf561a54d69c
Charles Riley <criley@xometry.com>
2019-09-06 13:52:42 -0400

    Actually produce umd

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ module.exports = {
   entry: './index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    libraryTarget: 'umd',
   }
 };
